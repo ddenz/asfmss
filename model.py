c1c1e2a894ba94bd157b096086afa902a7004398
ddnez <andre.bittar@gmail.com>
2021-06-21 15:47:22 +0100

    Fit each model and save in dictionary.

--- model.py
+++ model.py
@@ -1,23 +1,9 @@
 from keras.layers import Attention, Bidirectional, Conv1D, Dense, Embedding, Dropout, Input, LSTM, MaxPooling1D, TimeDistributed
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.optimizers import Adam
 from utils import prepare_sequential
 from utils import SEED, LABELS, MAX_LENGTH
 from sklearn.model_selection import train_test_split
-
-
-class SentenceEncoder(Sequential):
-    def __init__(self, embedding_matrix):
-        super().__init__()
-        self.embedding_matrix = embedding_matrix
-
-    def build_model(self, optimizer=Adam(lr=0.001), loss='categorical_crossentropy'):
-        # input_dim = vocab size, output_dim = embedding size, input_length = sentence length
-        self.add(Embedding(input_dim=self.embedding_matrix.shape[0], output_dim=self.embedding_matrix[0].shape[0],
-                           input_length=MAX_LENGTH, weights=[self.emb_matrix], trainable=False))
-        self.add(Conv1D(filters=64, kernel_size=7))
-        self.add(MaxPooling1D())
-        self.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
 
 def build_model(embedding_matrix):
@@ -30,7 +16,7 @@ def build_model(embedding_matrix):
     output = Dense(1)(do1)
 
     model = Model(inputs=inputs, outputs=output)
-    model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy') # added this
+    model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy')
 
     return model
 
@@ -42,6 +28,9 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
     #X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size=0.25, random_state=SEED)
 
-    m = build_model(emb_matrix)
-
-    m.fit(X_train, y_train[LABELS[0]])
+    models = {}
+    for label in LABELS:
+        m = build_model(emb_matrix)
+        history = m.fit(X_train, y_train[label])
+        models[label] = [m, history]
+        print(history)
