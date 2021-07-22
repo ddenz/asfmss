eb0cfcde96ec138a02bda4ebae4f03320a395b66
ddnez <andre.bittar@gmail.com>
2021-07-22 18:18:55 +0100

    Made sure input names are used in training loop.

--- model.py
+++ model.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     tf_train, tf_test, af_train, af_test, y_train, y_test = train_test_split(tf, af, y, test_size=0.2, random_state=SEED)
     # X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size=0.25, random_state=SEED)
 
-    tf_inputs = Input(shape=(MAX_LENGTH,))
-    af_inputs = Input(shape=af[0].shape)
+    tf_inputs = Input(shape=(MAX_LENGTH,), name='tf_inputs')
+    af_inputs = Input(shape=af[0].shape, name='af_inputs')
 
     tf_emb = Embedding(input_dim=emb_matrix.shape[0], output_dim=emb_matrix[0].shape[0], input_length=MAX_LENGTH,
                        weights=[emb_matrix], trainable=False)(tf_inputs)
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     model.summary()
 
     model.fit(
-        {'tf': tf_train, 'af': af_train},
+        {'tf_inputs': tf_train, 'af_inputs': af_train},
         {'ee': y_train[['t2_ee']]},
         epochs=2,
         batch_size=32
