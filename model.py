66dd1ac293ef64c06f05b3ca6d016b2363dce4ef
ddnez <andre.bittar@gmail.com>
2021-09-03 15:23:18 +1200

    Changed tf imports to tensorflow

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import logging
-import tensorflow as tf
+import tensorflow
 
 from tensorflow.keras.layers import Bidirectional, Dense, Embedding, Dropout, Input, LSTM, concatenate
 from tensorflow.keras.models import Model
@@ -25,19 +25,19 @@ class Attention(Model):
         # hidden shape == (batch_size, hidden size)
         # hidden_with_time_axis shape == (batch_size, 1, hidden size)
         # we are doing this to perform addition to calculate the score
-        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        hidden_with_time_axis = tensorflow.expand_dims(hidden, 1)
 
         # score shape == (batch_size, max_length, 1)
         # we get 1 at the last axis because we are applying score to self.V
         # the shape of the tensor before applying self.V is (batch_size, max_length, units)
-        score = tf.nn.tanh(
+        score = tensorflow.nn.tanh(
             self.W1(features) + self.W2(hidden_with_time_axis))
         # attention_weights shape == (batch_size, max_length, 1)
-        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+        attention_weights = tensorflow.nn.softmax(self.V(score), axis=1)
 
         # context_vector shape after sum == (batch_size, hidden_size)
         context_vector = attention_weights * features
-        context_vector = tf.reduce_sum(context_vector, axis=1)
+        context_vector = tensorflow.reduce_sum(context_vector, axis=1)
         return context_vector, attention_weights
 
 
